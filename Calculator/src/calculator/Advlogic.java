254c2a7d4888cf80c2ed6c64e2b9dfd895de9cb6
megaman2528 <freeze@archlinux.localeddomain>
2019-05-20 16:38:46 +0000

    sysout(branch2)

--- Calculator/src/calculator/Advlogic.java
+++ Calculator/src/calculator/Advlogic.java
@@ -1,10 +1,16 @@
 package calculator;
 
+
 public class Advlogic {
 	/* Plans a head
 	 * can do basic 2d geometry
 	 */
 	public void hello() {
 		System.out.println("hello branch");
+		
+	
+	}
+	public void branch2() {
+		System.out.println("branch 2");
 	}
 }
