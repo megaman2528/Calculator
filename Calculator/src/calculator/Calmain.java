78371ffcbc64469551a0182c21f25b045e9184eb
megaman2528 <megaman2528@gmail.com>
2019-05-21 14:51:17 +0000

    made some changes

--- Calculator/src/calculator/Calmain.java
+++ Calculator/src/calculator/Calmain.java
@@ -2,53 +2,69 @@ package calculator;
 
 import java.util.Scanner;
 
+
+
 public class Calmain {
-	private static int option;
+
 	private static double fsnumber, scnumber;
+	static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner RTF = new Scanner(System.in);
+
 		String run = "yes";
 		while (run.equals("yes")) {
-			Scanner userinput = new Scanner(System.in);
+			//Scanner input = new Scanner(System.in);
 			System.out.println("welcome");
-			System.out.println("Enter a number");
-			fsnumber = userinput.nextDouble();
+			/*System.out.println("Enter a number");
+			//fsnumber = userinput();//.nextDouble();
 
 			System.out.println("Enter a number");
-			scnumber = userinput.nextDouble();
-
+			*/ userinput();
 			System.out.println("what operation you want to do\n" + "enter 1 to add\n" + "enter 2 to subtract\n"
 					+ "enter 3 to multiply\n" + "enter 4 to divide\n");
 
 			calmethod();
-			run = userinput.next().toLowerCase();
-			System.out.println("program closed");
-		}
+			 run = input.next().toLowerCase();
 
+		}
+		System.out.println("program closed");
+
+	}
+	
+	public static void userinput() {
+		//Scanner inputsScanner = new Scanner(System.in);
+		System.out.println("Enter a number");
+		fsnumber = input.nextDouble();
+
+		System.out.println("Enter a number");
+		scnumber = input.nextDouble();	
+		
+		
+		
 	}
 
 	public static void calmethod() {
 		Scanner useroption = new Scanner(System.in);
+		int option;
 		option = useroption.nextInt();
-		Callogic calogiCallogic = new Callogic();
+		Callogic calogic = new Callogic();
 
 		switch (option) {
 		case 1:
-			System.out.println(calogiCallogic.add(fsnumber, scnumber));
+			System.out.println(calogic.add(fsnumber, scnumber));
 
 			break;
 
 		case 2:
-			System.out.println(calogiCallogic.subtract(fsnumber, scnumber));
+			System.out.println(calogic.subtract(fsnumber, scnumber));
 
 			break;
 		case 3:
-			System.out.println(calogiCallogic.multiply(fsnumber, scnumber));
+			System.out.println(calogic.multiply(fsnumber, scnumber));
 
 			break;
 		case 4:
-			System.out.println(calogiCallogic.divide(fsnumber, scnumber));
+			System.out.println(calogic.divide(fsnumber, scnumber));
 
 			break;
 
